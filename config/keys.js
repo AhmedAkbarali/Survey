b2a5e8680e2901bbf1c5b34f139ba0f576faeadd
AhmedAkbarali <ahmed.amin.akbarali@gmail.com>
2019-06-26 17:52:53 -0400

    auth flow completed

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//Logic to Determine which set of credentials to use
+if (process.env.NODE_ENV === 'production') {
+    //return prod keys
+    module.exports = require('./prod');
+} else {
+    //return dev keys
+    module.exports = require('./dev');
+}
